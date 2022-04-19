b8416ae42b158f3b745d16bdad427ddc16103b5c
Jake <castiglianoj@my.lanecc.edu>
2022-04-19 14:17:20 -0700

    first try

---
+++ BinaryNumbersQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryNumbersQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter #");
+            Queue<int> binaryQ = new Queue<int>();
+            binaryQ.Enqueue(1);
+            binaryQ.Enqueue(0);
+
+            if (!int.TryParse(Console.ReadLine(), out int userNum))
+            {
+                switch (userNum)
+                {
+                    //case = 0:
+
+                            
+                }
+            }
+        }
+
+       
+    }
+}
---
+++ StringReverseStack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringReverseStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your string");
+            string word = Console.ReadLine();
+            Stack<char> sWords = new Stack<char>();
+            foreach (char c in word)
+                sWords.Push(c);
+            foreach (char c in word)
+                Console.Write(sWords.Pop());
+        }
+
+    }
+}
